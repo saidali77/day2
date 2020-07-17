31adbec499bbe57086179e85284a78193f5ea08b
saidali77 <mamedovsaidali7@gmail.com>
2020-07-17 16:24:01 +0600

    test3

---
+++ task3.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+guests = ['Saidali' , 'Abdulla' , 'Imran']
+print('Я приглашаю на обед ' + guests[0])
+print('Я приглашаю на обед ' + guests[1])
+print('Я приглашаю на обед ' + guests[2])
